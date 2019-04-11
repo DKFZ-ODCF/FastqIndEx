a2d12843c8fda2f9feec5748d641efb88d908d3a
heinold <m.heinold@dkfz-heidelberg.de>
2019-04-11 09:27:22 +0200

    Add example script for mass tests
    The files are working on our LSF environments.
    
    Basically you call fqimasstestLSF.sh with a list of FASTQ files and an
    output directory to where the index files will be written.
    
    The script fqitest.sh is called for each FASTQ. Note, that the script
    will extract the FASTQ to the local scratch storage.
    
    Keep in mind, that these scripts are more an example and will not
    necessarily work on your platform.

---
+++ test/scripts/fqimasstestLSF.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -uvex
+
+declare listOfFiles="$1"
+declare baseOutputFolder="$2"
+
+[[ -z "$listOfFiles" ]] && echo "List of FASTQ files is empty. You need to call the masstest with [file with a list of fastq files] and [base output folder]." && exit 1
+
+[[ -z "$baseOutputFolder" ]] && echo "There was no output folder present. You need to call the masstest with [file with a list of fastq files] and [base output folder]. " && exit 2
+
+[[ ! -f "$listOfFiles" ]] && echo "File does not exist" && exit 3
+
+[[ ! -d "$baseOutputFolder"]] && echo "Base output folder does not exist, please create it." && exit 4
+
+for f in `cat $listOfFiles`; do
+	[[ -z "$f" ]] && continue
+	[[ ! -f "$f" ]] && continue
+
+	declare targetFile="$2"/"$f"
+	declare targetDirectory=$(dirname $targetFile)
+	echo $targetDirectory
+	mkdir -p "$targetDirectory"
+	FASTQ=$f INDEX=$targetFile bsub -W 1200 -n 1 -R "span[hosts=1]" -cwd $HOME -J FQIndExTest -R "rusage[mem=512]" -M 512 ~/fqitest.sh
+done

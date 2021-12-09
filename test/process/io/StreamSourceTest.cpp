49467a571b07dd0438cddcf37b7be504f656c0fb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-09 12:21:18 +0800

    Delete test directory

--- test/process/io/StreamSourceTest.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (c) 2019 DKFZ - ODCF
- *
- * Distributed under the MIT License (license terms are at https://github.com/dkfz-odcf/FastqIndEx/blob/master/LICENSE.txt).
- */
-
-// Some tests are in SourceTest. Need to move them here and add more.
